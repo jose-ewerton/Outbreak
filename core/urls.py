bc58d919aff7c817dd4cb4ac2c1ce9c2af0d4ec8
jose-ewerton <jose.ewerton@escolar.ifrn.edu.br>
2022-05-26 08:39:50 -0300

    edit urls.py, views,py and templates

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,17 @@
 
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, LoginView, RegisterView, FeedView, CreateNarrative, ListNarrative, DetailNarrative
 
 urlpatterns = [
+    #common pages 
     path('', HomeView.as_view(), name = 'home'),
+    path('login/', LoginView.as_view(), name = 'login' ),
+    path('register/', RegisterView.as_view(), name = 'register'),
+    path('feed', FeedView.as_view(), name = 'feed' ),
+    # CRUD pages
+    path('create-narrative/', CreateNarrative.as_view(), name = 'create-narrative' ),
+    path('list-narrative/', ListNarrative.as_view(), name = 'list-narrative' ),
+    path('detail-narrative/<int:pk>/', DetailNarrative.as_view(), name = 'detail-narrative'),
+    path('edit-narrativa/:<int:pk>',  ),
+    path('delete-narrativa/:<int:pk>', ),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+    
+class LoginView(TemplateView):
+    template_name = 'login.html'
+    
+class RegisterView(TemplateView):
+    template_name = 'register.html'
+    
+class FeedView(TemplateView):
+    template_name = 'feed.hml'
+
+class CreateNarrative(CreateView):
+    models = ''
+    fields = []
+    
+class ListNarrative(ListView):
+    model = ''
+
+class DetailNarrative(DetailView):
+    models = ''
+
+
